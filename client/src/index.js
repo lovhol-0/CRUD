509a093e04e387d59f938fa6ca924fc3b37a6275
Jocke <zedjay88@gmail.com>
2022-05-06 16:01:41 +0200

    Login funkar

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Login from './Pages/Login/Login';
 import RegisterUser from './Pages/Register/Register'
 import reportWebVitals from './reportWebVitals';
 
