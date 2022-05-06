04047d639e4afaceefa5ad84afeb98f5b0616de0
Hermaneken <Hermanekett@gmail.com>
2022-05-06 16:06:38 +0200

    design registrering 2.0

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import RegisterUser from './Pages/Register/Register'
 import reportWebVitals from './reportWebVitals';
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
