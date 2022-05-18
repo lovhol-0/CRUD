f2788b56c9951a4809d6bc8147a8d0768bc12531
Jocke <zedjay88@gmail.com>
2022-05-18 14:29:13 +0200

    funkar ej

--- client/src/routes.js
+++ client/src/routes.js
@@ -6,11 +6,13 @@ import Arkiv from "./pages/arkiv/arkiv";
 import Kontakt from "./pages/kontakt/kontakt";
 import RegisterUser from "./pages/register/register";
 import Login from "./pages/login/login";
-// import Inlogg from "./pages/inlogg/inlogg";
+import InloggadLayout from "./pages/layout/inloggadLayout";
+import {loginStatus} from "./pages/login/login";
 // import Sokresultat from "./pages/sokresultat/sokresultat";
 // import Sokish from "./pages/sokish/sokish";
 
 const ProjectRoutes = () => {
+
     return (
       <Router>
         <Routes>
@@ -19,14 +21,21 @@ const ProjectRoutes = () => {
                 <Route path="arkiv" element={<Arkiv />} />
                 <Route path="kontakt" element={<Kontakt />} />
                 <Route path="login" element={<Login />} />
-                <Route path="register" element={<RegisterUser />} />
-                {/* <Route path="inlogg" element={<Inlogg />} /> */}
+                <Route path="register" element={<RegisterUser />} /> 
                 {/* <Route path="sokresultat" element={<Sokresultat />} /> */}
                 {/* <Route path="sokish" element={<Sokish />} /> */}
             </Route>
+             <Route path="inloggad" element={<InloggadLayout />}> 
+             <Route path="inloggad/arkiv" element={<Arkiv />} />
+             <Route path="inloggad/kontakt" element={<Kontakt />} />
+             <Route index element={<Hem />} />
+             
+             /</Route>
         </Routes>
       </Router>
+
     );
-  };
+
+}; 
   
   export default ProjectRoutes;
